9d67f6a02d8fb46957d5e6f440a23c1f8992d110
leosalustri <leonardo.salustri@live.it>
2021-04-25 10:26:15 +0200

    Update bmp388Interrupt.ino

--- bmp388Interrupt/bmp388Interrupt.ino
+++ bmp388Interrupt/bmp388Interrupt.ino
@@ -7,22 +7,51 @@
 #define BMP388_INT 20
 #define BMP388_SPI_SPEED 1000000
 
-#define BMP388_REG_INT1_CTRL 0x0D
+#define BMP388_REG_WHO_AM_I 0x00
+#define BMP388_REG_STATUS 0x03
+#define BMP388_REG_OUTP 0x04 //primo byte pressione, sono 3
+#define BMP388_REG_OUTT 0x07 //primo byte temperatura, sono 3
+
+#define BMP388_REG_INT_CTRL 0x19  //01000010
+#define BMP388_REG_PWR_CTRL 0x1B  //00110011
+#define BMP388_REG_OSR 0x1C  //00001010  oversampling press 2x  temp 2x
+#define BMP388_REG_ODR 0x1D  //00000001  100Hz
+#define BMP388_REG_CONFIG 0x1F  //00000010 iir filter coeff 1
+
 
 //booleani di appoggio
 volatile bool readyBaro = 0;
 bool baroRead = 0;
 
-//variabili per convertire i valori letti dal sensore in SI, il primo valore delle espressioni dipende dalla scala vedi il datasheet
-const float BMP388BitPascal =  ; 
-const float BMP388BitToC = ; 
 float pressure; //in Pa
 float temp; //unità misura da decidere
-int16_t rawPressure, rawTemp;
+uint32_t rawPressure, rawTemp;
 
 //variabili per il timing
 
 unsigned long prevMillis = 0;
+
+struct BMP388_calib_data{
+
+    double par_t1;
+    double par_t2;
+    double par_t3;
+    double par_p1;
+    double par_p2;
+    double par_p3;
+    double par_p4;
+    double par_p5;
+    double par_p6;
+    double par_p7;
+    double par_p8;
+    double par_p9;
+    double par_p10;
+    double par_p11;
+    double t_lin;
+  
+};
+
+struct BMP388_calib_data calData;
 
 void setup() {
 
@@ -41,31 +70,9 @@ void setup() {
   //inizializzo SPI e faccio sapere che verrà usata all'interno degli interrupt
   SPI.begin();
 
-  SPI.beginTransaction(SPISettings(BMP388_SPI_SPEED, MSBFIRST, SPI_MODE3));
-  digitalWrite(BMP388_CS, LOW);
-  uint8_t lallero[3] = {BMP388_REG_INT1_CTRL | 0b10000000, 0x02, 0x01};
-  SPI.transfer(lallero, 3);
-  digitalWrite(BMP388_CS, HIGH);
-  SPI.endTransaction();
-  Serial.print("valori interrupt: ");
-  Serial.print(lallero[1], BIN);
-  Serial.print("   ");
-  Serial.println(lallero[2], BIN);
-  
   Serial.println("setto imu");
   
   BaroSetup();
-
-  SPI.beginTransaction(SPISettings(BMP388_SPI_SPEED, MSBFIRST, SPI_MODE3));
-  digitalWrite(BMP388_CS, LOW);
-  uint8_t laller[3] = {BMP388_REG_INT1_CTRL | 0b10000000, 0x02, 0x01};
-  SPI.transfer(laller, 3);
-  digitalWrite(BMP388_CS, HIGH);
-  SPI.endTransaction();
-  Serial.print("valori interrupt: ");
-  Serial.print(laller[1], BIN);
-  Serial.print("   ");
-  Serial.println(laller[2], BIN);
   
   SPI.beginTransaction(SPISettings(BMP388_SPI_SPEED, MSBFIRST, SPI_MODE3));
   digitalWrite(BMP388_CS, LOW);
@@ -88,7 +95,7 @@ void loop() {
     readyBaro = 0;
     BaroGetPress();
     BaroGetTemp();
-    gyroRead = 1;
+    baroRead = 1;
   }
 
   if(millis() - prevMillis >= 500){
@@ -106,59 +113,129 @@ void BaroSetup(){
   SPI.beginTransaction(SPISettings(BMP388_SPI_SPEED, MSBFIRST, SPI_MODE3));
   digitalWrite(BMP388_CS, LOW);
   //pin interrupt sensore
-  byte intBuff[3] = {BMP388_REG_INT1_CTRL, 0x02, 0x01}; //indirizzo int1, valore int1 gyro, valore int2 acc
-  SPI.transfer(intBuff, 3);
+  byte settingsBuff[10] = {BMP388_REG_INT_CTRL, 0x42, BMP388_REG_PWR_CTRL, 0x33,
+                            BMP388_REG_OSR, 0x0A, BMP388_REG_ODR, 0x01, BMP388_REG_CONFIG, 0x02,};
+  SPI.transfer(settingsBuff, 10);
   digitalWrite(BMP388_CS, HIGH);
-
-  digitalWrite(BMP388_CS, LOW);
-  //Data Rates e Full Scale accelerometro e gyro
-  byte odrScaleBuff[3] = {BMP388_REG_CTRL1_XL, 0b01110100, 0b01111100}; //vedi CTRL1_XL e CTRL2_G, 833Hz, 32g,acc lpf2 disattivo, 2000dps
-  SPI.transfer(odrScaleBuff,3);
-  digitalWrite(BMP388_CS, HIGH);
-
-  digitalWrite(BMP388_CS, LOW);
-  //dataReady mask
-  byte dRBuff[2] = {BMP388_REG_CTRL4_C, 0b0000000};
-  SPI.transfer(dRBuff, 2);
-  digitalWrite(BMP388_CS, HIGH);
-  
   SPI.endTransaction();
+
+  BaroGetCalibParam();
   
 }
 
 void BaroGetPress(){
 
-  byte buff[2];
+  byte buff[3];
   SPI.beginTransaction(SPISettings(BMP388_SPI_SPEED, MSBFIRST, SPI_MODE3));
   digitalWrite(BMP388_CS, LOW);
   
-  SPI.transfer(BMP388_REG_OUT_TEMP_L | 0b10000000);
-  SPI.transfer(buff,2);
+  SPI.transfer(BMP388_REG_OUTP | 0b10000000);
+  SPI.transfer(buff,3);
 
   digitalWrite(BMP388_CS, HIGH);
   SPI.endTransaction();
-  rawTemp = buff[1] << 8 | buff[0];
-  temp = rawTemp * BMP388BitToC;
+  rawPressure = buff[2] <<16 | (buff[1] << 8 | buff[0]);
+  pressure = BMP388_compensate_pressure(rawPressure, &calData);
   
 }
 
 void BaroGetTemp(){
 
-  byte buff[2];
+  byte buff[3];
   SPI.beginTransaction(SPISettings(BMP388_SPI_SPEED, MSBFIRST, SPI_MODE3));
   digitalWrite(BMP388_CS, LOW);
   
-  SPI.transfer(BMP388_REG_OUT_TEMP_L | 0b10000000);
-  SPI.transfer(buff,2);
+  SPI.transfer(BMP388_REG_OUTT | 0b10000000);
+  SPI.transfer(buff,3);
 
   digitalWrite(BMP388_CS, HIGH);
   SPI.endTransaction();
-  rawTemp = buff[1] << 8 | buff[0];
-  temp = rawTemp * BMP388BitToC;
+  rawTemp = buff[2] <<16 | (buff[1] << 8 | buff[0]);
+  temp = BMP388_compensate_temperature(rawTemp, &calData);
   
 }
 
 void IntB(){
   readyBaro = 1;
   //baroRead = 1;
+}
+
+void BaroGetCalibParam(){
+
+  SPI.beginTransaction(SPISettings(BMP388_SPI_SPEED, MSBFIRST, SPI_MODE3));
+  digitalWrite(BMP388_CS, LOW);
+  byte buff[21];
+  SPI.transfer(0x31 | 0b10000000);
+  SPI.transfer(buff, 21);
+  digitalWrite(BMP388_CS, HIGH);
+  SPI.endTransaction();
+
+  uint16_t REG_par_t1 = buff[1] << 8 | buff[0];
+  uint16_t REG_par_t2 = buff[3] << 8 | buff[2];
+  int8_t REG_par_t3 = buff[4];
+  int16_t REG_par_p1 = buff[6] << 8 | buff[5];
+  int16_t REG_par_p2= buff[8] << 8 | buff[7];
+  int8_t REG_par_p3 = buff[9];
+  int8_t REG_par_p4 = buff[10];
+  uint16_t REG_par_p5 = buff[12] << 8 | buff[11];
+  uint16_t REG_par_p6 = buff[14] << 8 | buff[13];
+  int8_t REG_par_p7 = buff[15];
+  int8_t REG_par_p8 = buff[16];
+  int16_t REG_par_p9 = buff[18] << 8 | buff[17];
+  int8_t REG_par_p10 = buff[19];
+  int8_t REG_par_p11 = buff[20];
+
+  calData.par_t1 = (double)REG_par_t1 / pow(2, -8);
+  calData.par_t2 = (double)REG_par_t2 / pow(2, 30);
+  calData.par_t3 = (double)REG_par_t3 / pow(2, 48);
+  calData.par_p1 = ((double)REG_par_p1 - pow(2, 14)) / pow(2, 20);
+  calData.par_p2 = ((double)REG_par_p2 - pow(2, 14)) / pow(2, 29);
+  calData.par_p3 = (double)REG_par_p3 / pow(2, 32);
+  calData.par_p4 = (double)REG_par_p4 / pow(2, 37);
+  calData.par_p5 = (double)REG_par_p5 / pow(2, -3);
+  calData.par_p6 = (double)REG_par_p6 / pow(2, 6);
+  calData.par_p7 = (double)REG_par_p7 / pow(2, 8);
+  calData.par_p8 = (double)REG_par_p8 / pow(2, 15);
+  calData.par_p9 = (double)REG_par_p9 / pow(2, 48);
+  calData.par_p10 = (double)REG_par_p10 / pow(2, 48);
+  calData.par_p11 = (double)REG_par_p11 / pow(2, 65);
+  
+}
+
+
+static float BMP388_compensate_temperature(uint32_t uncomp_temp, struct BMP388_calib_data *calib_data){
+  float partial_data1;
+  float partial_data2;
+  partial_data1 = (float)(uncomp_temp - calib_data->par_t1);
+  partial_data2 = (float)(partial_data1 * calib_data->par_t2);
+  calib_data->t_lin = partial_data2 + (partial_data1 * partial_data1) * calib_data->par_t3;
+  return calib_data->t_lin;
+}
+
+static float BMP388_compensate_pressure(uint32_t uncomp_press, struct BMP388_calib_data *calib_data){
+  float comp_press;
+  float partial_data1;
+  float partial_data2;
+  float partial_data3;
+  float partial_data4;
+  float partial_out1;
+  float partial_out2;
+
+  partial_data1 = calib_data->par_p6 * calib_data->t_lin;
+  partial_data2 = calib_data->par_p7 * (calib_data->t_lin * calib_data->t_lin);
+  partial_data3 = calib_data->par_p8 * (calib_data->t_lin * calib_data->t_lin * calib_data->t_lin);
+  partial_out1 = calib_data->par_p5 + partial_data1 + partial_data2 + partial_data3;
+
+  partial_data1 = calib_data->par_p2 * calib_data->t_lin;
+  partial_data2 = calib_data->par_p3 * (calib_data->t_lin * calib_data->t_lin);
+  partial_data3 = calib_data->par_p4 * (calib_data->t_lin * calib_data->t_lin * calib_data->t_lin);
+  partial_out2 = (float)uncomp_press * (calib_data->par_p1 + partial_data1 + partial_data2 + partial_data3);
+
+  partial_data1 = (float)uncomp_press * (float)uncomp_press;
+  partial_data2 = calib_data->par_p9 + calib_data->par_p10 * calib_data->t_lin;
+  partial_data3 = partial_data1 *  partial_data2;
+  partial_data4 = partial_data3 + ((float)uncomp_press * (float)uncomp_press * (float)uncomp_press) * calib_data->par_p11;
+  comp_press = partial_out1 + partial_out2 + partial_data4;
+
+  return comp_press;
 }
